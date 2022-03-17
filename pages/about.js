35e295bc2a967f0c045334f3496d00be3d0a4afd
sonphan15 <son.phan@levinci.group>
2022-03-17 10:57:21 +0700

    updating movie ui

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import ReactPlayer from 'react-player';
+import React from 'react';
+export default function About(props) {
+
+
+    return (
+        <div>
+            <ReactPlayer 
+            url="https://dash.megacdn.xyz/raw/0dad3b25ffe465c68d4e62a21adc522d/index.m3u8" controls/>
+
+        </div>
+    );
+}
