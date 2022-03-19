e9923e2bd4f81bda6332d4b8c1e2d1a13472a1de
sonphan15 <son.phan@levinci.group>
2022-03-19 17:31:41 +0700

    another test

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,14 @@
 import ReactPlayer from 'react-player';
 import React from 'react';
+import Layout from '../components/layout/layout';
 export default function About(props) {
 
 
     return (
-        <div>
+        <Layout>
             <ReactPlayer 
             url="https://dash.megacdn.xyz/raw/0dad3b25ffe465c68d4e62a21adc522d/index.m3u8" controls/>
+        </Layout>
 
-        </div>
     );
 }
