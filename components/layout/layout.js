b9cd5b3358191f62406244d2c9d32e330ee7f50a
sonphan15 <son.phan@levinci.group>
2022-03-19 17:23:14 +0700

    update meta

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Header from "./header";
 
 
 const Layout = ({children}) => {
     return(
         <>
+            <Head>
+                <meta name="referrer" content="no-referrer" />
+            </Head>
             <div className="flex flex-col min-h-screen">
                 <Header />
                 <main style={{minHeight: 'calc(100vh - 726px)'}}>{children}</main>
