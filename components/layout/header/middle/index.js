f04d53a5ac47c72269912d1475feb6c98f0d618a
sonphan15 <son.phan@levinci.group>
2022-04-11 19:48:08 +0700

    fix build

--- components/layout/header/middle/index.js
+++ components/layout/header/middle/index.js
@@ -1,4 +1,4 @@
-import SlideShow from "../../../SlideShow";
+import SlideShow from '../../../SlideShow';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faStar } from "@fortawesome/free-solid-svg-icons";
 const img = [1, 2, 3, 4];
